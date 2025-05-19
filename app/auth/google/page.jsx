792a4117f74894978db2dc7fb2c367b9499f42d4
Pratham vig <spark2009971@gmail.com>
2025-05-19 14:53:58 +0530

    Update page.jsx

--- app/auth/google/page.jsx
+++ app/auth/google/page.jsx
@@ -1,27 +1,20 @@
-'use server'
+import { NextResponse } from 'next/server';
+import { OAuth2Client } from 'google-auth-library';
 
-import { redirect } from 'next/navigation'
-import { OAuth2Client } from "google-auth-library";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
-
-export async  function google() {
-  let client=new OAuth2Client(
+const client = new OAuth2Client(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  process.env.BASE_URI+'auth/google/callback'
+  process.env.BASE_URI + '/auth/google/callback'
 );
-  let url = client.generateAuthUrl({
-  access_type: "offline",
-  scope: [
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/userinfo.email",
-    // "https://www.googleapis.com/auth/calendar",
-    // "https://mail.google.com/",
-  ],
-});
-redirect(url);
+
+export async function GET() {
+  const url = client.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email"
+    ]
+  });
+
+  return NextResponse.redirect(url);
 }
