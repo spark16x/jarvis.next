aa1c8acade41ef94160fd47972952600b9e338c1
spark16x <spark2009971@gmail.com>
2025-05-19 19:43:32 +0530

    update

--- app/auth/google/route.js
+++ app/auth/google/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { OAuth2Client } from 'google-auth-library';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const client = new OAuth2Client(
