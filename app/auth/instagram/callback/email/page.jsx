55547c707bcb190e831673bcf56014018d76bfce
spark16x <spark2009971@gmail.com>
2025-05-24 19:24:08 +0530

    update page.jsx

--- app/auth/instagram/callback/email/page.jsx
+++ app/auth/instagram/callback/email/page.jsx
@@ -1,12 +1,33 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
-dotenv.config();
 
 export default function Email() {
-  let params =useSearchParams();
-  // let user=jwt.verify(params.get('user'), process.env.SUPABASE_KEY)
-  return (<div>name </div>)
+  const [user, setUser] = useState(null);
+  let params = useSearchParams();
+  
+  useEffect(() => {
+    const token = params.get('user');
+    if (token) {
+      try {
+        // Decoding token or verifying the token could be better done server-side
+        const decodedUser =  jwt.verify(token, process.env.SUPABASE_KEY) 
+        setUser(decodedUser);
+      } catch (error) {
+        console.error('Invalid token', error);
+      }
+    }
+  }, [params]);
+
+  return (
+    <div>
+      {user ? (
+        <p>Welcome, {user.name}</p> // Adjust according to decoded token structure
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
