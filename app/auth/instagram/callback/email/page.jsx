8b92b9e88ddeddda4c591c1d118edb04b94a428b
spark16x <spark2009971@gmail.com>
2025-05-24 18:28:41 +0530

    update page.jsx

--- app/auth/instagram/callback/email/page.jsx
+++ app/auth/instagram/callback/email/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
+import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 dotenv.config();
 
