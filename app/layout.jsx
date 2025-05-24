9cb81adf1bf55577c26d24d8770e0e901f7e1645
spark16x <spark2009971@gmail.com>
2025-05-24 13:14:23 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,33 +19,6 @@ export const metadata = {
     description: 'Jarvis is a ai aegant Developed by Pratham vig',
     url: 'https://jarvisnext.vercel.app',
     siteName: 'J.A.R.V.I.S',
-    images: [
-      {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
-    videos: [
-      {
-        url: 'https://nextjs.org/video.mp4', // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-    ],
-    audio: [
-      {
-        url: 'https://nextjs.org/audio.mp3', // Must be an absolute URL
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
+    }
 
 }
