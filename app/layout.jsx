657432c1b48b4f7ea86f8e04ddb9e9e5650233a0
spark16x <spark2009971@gmail.com>
2025-05-24 12:45:04 +0530

    update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,16 +14,38 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "J.A.R.V.I.S",
-    description: "Jarvis is a ai aegant Developed by Pratham vig",
-};
+    description: "Jarvis is a ai aegant Developed by Pratham vig",openGraph: {
+    title: 'J.A.R.V.I.S',
+    description: 'Jarvis is a ai aegant Developed by Pratham vig',
+    url: 'https://jarvisnext.vercel.app',
+    siteName: 'J.A.R.V.I.S',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    videos: [
+      {
+        url: 'https://nextjs.org/video.mp4', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    audio: [
+      {
+        url: 'https://nextjs.org/audio.mp3', // Must be an absolute URL
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-        <Analytics/>
-      </body>
-    </html>
-  );
 }
