1e33ca8b30c14f4c756e592130d635286666f3e6
spark16x <spark2009971@gmail.com>
2025-05-25 12:16:01 +0530

    update page.jsx

--- app/auth/instagram/callback/email/page.jsx
+++ app/auth/instagram/callback/email/page.jsx
@@ -10,16 +10,27 @@ export default function Email() {
   const [user, setUser] = useState(null);
   let params = useSearchParams();
   
-
-    const token = params.get('user');
-   
-        // Decoding token or verifying the token could be better done server-side
-        const decodedUser =  jwt.verify(token, process.env.SUPABASE_KEY) 
-        setUser(decodedUser);
-     
-    
- 
-
+  
+  const token = params.get('user');
+  
+  // Decoding token or verifying the token could be better done server-side
+  const decodedUser = jwt.verify(token, process.env.SUPABASE_KEY)
+  setUser(decodedUser);
+  
+  useEffect(() => { var script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/npm/eruda";
+    document.body.appendChild(script);
+    script.onload = function() { eruda.init() }() }, [])
+    useEffect(() => { var script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/npm/eruda";
+    document.body.appendChild(script);
+    script.onload = function() { eruda.init() }() }, [])
+    useEffect(() => { var script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/npm/eruda";
+    document.body.appendChild(script);
+    script.onload = function() { eruda.init() }() }, [])
+  
+  
   return (
     <div>
       {user ? (
