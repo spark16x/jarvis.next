9e4a7213df8880b047a28364ddfe3dfa0e72dc83
Spark16x <spark2009971@gmail.com>
2025-06-02 13:08:09 +0530

    update

--- app/auth/instagram/callback/email/middleware.js
+++ app/auth/instagram/callback/email/middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function Middleware() {
   let res =NextResponse.next();
+  console.log('mid is ok')
   res.jaon({mid:true})
   return res
 }
