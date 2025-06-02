17911ad9c6aac5efed578ba226ab8ee2f50a6a6a
Spark16x <spark2009971@gmail.com>
2025-06-02 22:00:20 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -91,10 +91,10 @@ export default function Home() {
           <h2 className="text-3xl font-bold text-center mb-10">Powerful Features</h2>
           <div className="grid md:grid-cols-3 gap-8 text-center">
             <div>
-              <div onMouseEnter="{()=>{setVoiceHover(true)}}">
-                 voiceHover ?
-                 <video src="/videos/voice-contol.mp4" className="mx-auto mb-4 rounded" alt="Voice Control" autoplay />:
-                 <img src="/imgs/voice-contol.png" className="mx-auto mb-4 rounded" alt="Integrated Services" />
+              <div onMouseEnter="{()=>{setVoiceHover(true)}}" onMouseLeave="{()=>{setVoiceHover(false)}}">
+                 {voiceHover ?
+                 <video src="/videos/voice-contol.mp4" className="mx-auto mb-4 rounded" alt="Voice Control" autoPlay loop />:
+                 <img src="/imgs/voice-contol.png" className="mx-auto mb-4 rounded" alt="Integrated Services" />}
                  </div>
 
               <h3 className="text-2xl font-semibold mb-2">Voice Control</h3>
