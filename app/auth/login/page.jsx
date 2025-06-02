98f1cf0cbcf1b1be733b9a38f3e4c967bc065415
Spark16x <spark2009971@gmail.com>
2025-06-02 19:46:30 +0530

    update page.jsx

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
         <div className="auth-container border-1 border-gray-700 backdrop-blur-sm p-6 sm:p-8 rounded-xl shadow-lg animate-fade-in transition-all duration-300 hover:shadow-xl hover:scale-101 max-w-md w-full">
           <h2 className="text-white text-2xl font-bold mb-4 ">Login to Jarvis</h2>
 
-          <div className="relative mt-4 space-y-3">
+          <div className=" mt-4 space-y-3">
             {/* Google OAuth */}
             <Link href="/auth/google" className="block btn oauth-btn google-btn w-full flex items-center justify-center gap-2 py-2 px-3 rounded-md border border-gray-700 font-semibold text-gray-800 bg-white hover:bg-gray-100 transition-colors">
               <FontAwesomeIcon icon={faGoogle} className="w-5 h-5" />
@@ -47,7 +47,7 @@ const LoginPage = () => {
             </Link>
             
             <div className="border-t border-gray-700 my-4"></div>
-            <p className="absolute  text-gray-400 mb-2">Or continue with</p>
+            <p className=" -translate-y-16 text-gray-400 mb-2">Or continue with</p>
           </div>
         </div>
       </body>
