652bc8e9e85644d9b8680ad84b1cfcd6e9c8ce4f
Spark16x <spark2009971@gmail.com>
2025-06-02 12:46:06 +0530

    update page.jsx

--- app/auth/instagram/callback/email/page.jsx
+++ app/auth/instagram/callback/email/page.jsx
@@ -2,18 +2,20 @@
 
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import supabase from '@/components/supabase_client.js';
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 dotenv.config();
 
-export default function Email() {
+export default async function Email() {
 let params= useSearchParams();
-let id=params.get('id')
+let id=params.get('id');
+let user = await supabase.from('users_profile').select().eq('id', id).single();
 
   return (
     <div>
       {id ? (
-        <p>Welcome, {id}</p> // Adjust according to decoded token structure
+        <p>Welcome, {user.data.name}</p> // Adjust according to decoded token structure
       ) : (
         <p >Loading... </p>
       )}
