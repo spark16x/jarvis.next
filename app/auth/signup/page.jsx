8fb788a27aeee5f172226ecde8194363c0e57899
Spark16x <spark2009971@gmail.com>
2025-06-03 11:23:58 +0530

    update

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -32,9 +32,10 @@ const signupPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
       </Head>
-      <body className="bg-gray-900 text-gray-200 font-sans flex justify-center items-center h-screen text-center overflow-hidden">
-        <div className="stars"></div>
-        <div className="p-6 sm:p-8 rounded-xl shadow-lg animate-fade-in transition-all duration-300 hover:shadow-xl hover:scale-101 max-w-md w-full">
+      <body className="bg-[url(/imgs/bg-mobile.jpg)] md:bg-[url(/imgs/bg-desktop.jpg)] bg-cover bg-center bg-no-repeat text-gray-200 font-sans flex justify-center items-center h-screen text-center ">
+        
+        <div className="auth-container border-1 border-gray-700 backdrop-blur-sm p-6 sm:p-8 rounded-xl shadow-lg animate-fade-in transition-all duration-300 hover:shadow-xl hover:scale-101 max-w-md w-full"> 
+        
           <img src="/imgs/logo.png" alt="Jarvis Logo" className="logo w-16 mb-4 animate-pulse" />
           <h2 className="text-blue-400 text-2xl font-bold mb-4 shadow-sm">Sign up to Jarvis</h2>
 
