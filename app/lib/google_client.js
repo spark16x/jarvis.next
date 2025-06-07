09a0cf1df4aef519b353c53681cc89accaca00d5
Spark16x <spark2009971@gmail.com>
2025-06-07 13:19:07 +0530

    update google_client.js

--- app/lib/google_client.js
+++ app/lib/google_client.js
@@ -1,7 +1,10 @@
 import { OAuth2Client } from 'google-auth-library';
+import dotenv from "dotenv";
 
-export const client = new OAuth2Client(
+dotenv.config();
+const client = new OAuth2Client(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   process.env.BASE_URI + '/auth/google/callback'
 );
+export default client;
