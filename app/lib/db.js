f69d28d24936eee20deb19f38ec2d68e61072cbb
Spark16x <spark2009971@gmail.com>
2025-06-09 09:57:08 +0530

    update db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,13 +2,28 @@ import { Pool } from 'pg'
 import dotenv from "dotenv";
 
 dotenv.config();
- 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.POSTGRES_USER,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-})
 
-console.log(pool.connect())
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPgVersion();
