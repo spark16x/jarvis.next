42f3ed6ee3093eebb89d73c11a06f41e1f535100
Spark16x <spark2009971@gmail.com>
2025-06-09 16:48:37 +0530

    update route.js

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -10,7 +10,8 @@ export async function GET() {
     access_type: "offline",
     scope: [
       "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email"
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/user.birthday.read"
     ]
   });
   
