c2fdcbf3d76f92b18a793503708cc00185c6f9a4
Spark16x <spark2009971@gmail.com>
2025-06-09 17:24:41 +0530

    update page.jsx

--- app/auth/google/page.jsx
+++ app/auth/google/page.jsx
@@ -2,9 +2,9 @@ export default function LoginPage() {
   let url;
   fetch('https://jarvisnext.vercel.app/api/google')
     .then((v) => {
-      
+      url=v
       console.log(v)
     })
   
-  return (<div>Redricting { v }</div>)
+  return (<div>Redricting { url }</div>)
 }
