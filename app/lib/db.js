278fbc361eb2ec2167e1883266634b1de79fb398
Spark16x <spark2009971@gmail.com>
2025-06-09 09:32:13 +0530

    update db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -5,7 +5,7 @@ dotenv.config();
  
 const pool = new Pool({
   host: process.env.PGHOST,
-  user: PGUSER,
+  user: process.env.PGUSER,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
