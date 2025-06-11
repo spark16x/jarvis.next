e0807480b0c7142043ec0f20dc16a9f62c82cddc
Spark16x <spark2009971@gmail.com>
2025-06-11 19:52:37 +0530

    update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+dotenv.config();
 
-export default function middleware() {
-  console.log('mid is ok')
+export default function middleware(request) {
+  if (request.cookies.has('token')) {
+    let user = jwt.verify(req.cookies.token, process.env.SUPABASE_KEY)
+    console.log(user)
+  }
+  
   
   return NextResponse.next()
 }
