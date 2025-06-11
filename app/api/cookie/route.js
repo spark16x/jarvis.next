ba91f373489a20d37951d0d8080d1b03eaf39adf
Spark16x <spark2009971@gmail.com>
2025-06-11 19:16:33 +0530

    update route.js

--- app/api/cookie/route.js
+++ app/api/cookie/route.js
@@ -1,10 +1,11 @@
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   const token = searchParams.get('token');
-  let cookie= await cookies();
-  cookie.set("token", token,{ httpOnly: true, secure: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+  let cookie = await cookies();
+  cookie.set("token", token, { httpOnly: true, secure: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
   
-  return  NextResponse.redeict({token })
+  return NextResponse.redirect('/chat');
 }
