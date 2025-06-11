5a5d09018ca9da9ca4ce6ec90941f78352c42e1f
Spark16x <spark2009971@gmail.com>
2025-06-11 15:46:09 +0530

    update route.js

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,22 +1,28 @@
 import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 import pool from '@/app/lib/db.js';
+import { cookies } from 'next/headers';
 
 
 export async function POST(request) {
-  let client=await pool.connect();
+  let client = await pool.connect();
   const body = await headers();
- let name= body.get('name')
- let email= body.get('email')
- let password= body.get('password')
- 
- let user=await pool.query(`
- INSERT INTO auth.users(id, name, email, password, avatar)
-VALUES(gen_random_uuid(), '${name}', '${email}', '${password}', 'null')
+  let cookie = await cookies();
+  
+  let name = body.get('name')
+  let email = body.get('email')
+  let password = body.get('password')
+  let provider = body.get('provider')
+  let avatar = body.get('avatar')
+  
+  
+  let user = await pool.query(`
+ INSERT INTO auth.users(id, name, email, password, avatar,provider)
+VALUES(gen_random_uuid(), '${name}', '${email}', '${password}', '${avatar}','${provider}')
 RETURNING *`
-   
- )
- console.log(user)
-user=user.rows[0]
-  return Response.json({user})
+    
+  )
+  user = user.rows[0];
+  cookie.set("token", user, { httpOnly: true, secure: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+  return Response.json({ message: `registration is sussfull of the ${user.name}` })
 }
