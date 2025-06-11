58e5fa96a0a6d9bc2274ee3caa634b09084a305c
Spark16x <spark2009971@gmail.com>
2025-06-11 15:02:07 +0530

    update route.js

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,9 +1,20 @@
 import { NextResponse } from 'next/server';
-import { headers } from 'next/headers'
+import { headers } from 'next/headers';
+import db from '@/app/lib/db.js';
 
 
 export async function POST(request) {
   const body = await headers();
  let name= body.get('name')
-  return Response.json({ name })
+ let eamil= body.get('eamil')
+ let password= body.get('password')
+ 
+ let user=await db.query(`
+ INSERT INTO auth.users(id, name, email, password, avatar)
+VALUES(gen_random_uuid(), '${name}', '${email}', '${password}', 'null')
+RETURNING *`
+   
+ )
+
+  return Response.json()
 }
