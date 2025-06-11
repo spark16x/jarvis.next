af03b20465e608751ab9985fb44c8cfc46c45edb
Spark16x <spark2009971@gmail.com>
2025-06-11 19:05:34 +0530

    update route.js

--- app/api/cookie/route.js
+++ app/api/cookie/route.js
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers';
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   const token = searchParams.get('token');
+  let cookie= await cookies();
   cookie.set("token", token);
   return  Response.json({token })
 }
