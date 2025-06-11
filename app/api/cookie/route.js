f6ff2be7ecb555796fb4bff1e7e2a6491db44a2f
Spark16x <spark2009971@gmail.com>
2025-06-11 19:15:13 +0530

    update

--- app/api/cookie/route.js
+++ app/api/cookie/route.js
@@ -4,6 +4,7 @@ export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   const token = searchParams.get('token');
   let cookie= await cookies();
-  cookie.set("token", token);
-  return  Response.json({token })
+  cookie.set("token", token,{ httpOnly: true, secure: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+  
+  return  NextResponse.redeict({token })
 }
--- app/auth/google/page.jsx
+++ app/auth/google/page.jsx
@@ -1,9 +1,8 @@
 export default function LoginPage() {
   let url;
   fetch('https://jarvisnext.vercel.app/api/google')
-    .then((v) => {
-      url=v
-      console.log(v)
+    .then(async (v) => {
+      url =await v.text()
     })
   
   return (<div>Redricting { url }</div>)
