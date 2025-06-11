2dd1c7c93872166d6ec3ec7579c12214235c1e81
Spark16x <spark2009971@gmail.com>
2025-06-11 12:46:42 +0530

    update page.jsx

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -69,9 +69,7 @@ const signupPage = () => {
             <div className="border-t border-gray-700 my-4"></div>
             <p className=" -translate-y-[28px] text-gray-400 mb-2">Or continue with</p>
             
-            <form action={()=>{
-            console.log('Sign up data:', { name, email, password, confirmPassword });
-            }} className="mt-4 space-y-3">
+            <form action={handleSubmit} className="mt-4 space-y-3">
             <div>
               <input
                 type="text"
