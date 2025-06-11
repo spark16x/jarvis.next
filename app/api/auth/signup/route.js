04ca5448748bcdae8595e4d4d31d87d6d245ff9f
Spark16x <spark2009971@gmail.com>
2025-06-11 13:30:54 +0530

    update route.js

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
-  const formData = await request.formData()
-  const name = formData.get('name')
-  const email = formData.get('email')
-  return NextResponse.json({ name, email })
+  const res = await request.json()
+  return Response.json({ res })
 }
