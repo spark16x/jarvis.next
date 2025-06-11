7bd43f1f28a1d595958a5e57770ae28034c44dcc
Spark16x <spark2009971@gmail.com>
2025-06-11 13:02:34 +0530

    update route.js

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
-  console.log(req.body)
+  console.log(request.body)
   return NextResponse.json({body:request.body})
 }
