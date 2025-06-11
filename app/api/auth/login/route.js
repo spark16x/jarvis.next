9ffc7e23bf1de551ac6a309d26460ebc9863ca57
Spark16x <spark2009971@gmail.com>
2025-06-11 17:37:58 +0530

    update route.js

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -7,7 +7,12 @@ dotenv.config();
 export async function POST(request) {
  let {email, password} = await request.json();
  let user = await pool.query(`SELECT * FROM auth.users WHERE email = '${email}' AND password = '${password}' `)
- user = user.rows[0]
- console.log(user)
-return Response.json({user })
+ user = user.rows[0];
+ if (user) {
+  return Response.json({user })
+ } else {
+  return Response.json({message :'user is not found' })
+ }
+ // let token = jwt.sign(user, process.env.SUPABASE_KEY, { expiresIn: '720h' });
+
 }
