496a557aa8d04303609e5633aea7444166e88c80
Spark16x <spark2009971@gmail.com>
2025-06-12 13:59:12 +0530

    update

--- client/app/lib/db.js
+++ client/app/lib/db.js
@@ -1,4 +1,18 @@
+import { Pool } from 'pg'
+import dotenv from "dotenv";
+dotenv.config()
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
 
 
 export default pool;
