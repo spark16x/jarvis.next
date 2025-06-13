814fd3d1b246d988a8022dfd79b5a317cb0a40e9
Spark16x <spark2009971@gmail.com>
2025-06-13 19:01:31 +0530

    update

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -1,3 +1,3 @@
 export default function Chat() {
-  return (<div className="bg-gray-700 w-full h-full" >chat tab</div>)
+  return (<div className="bg-gray-700 w-screen h-screen" >chat tab</div>)
 }
--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} w-full h-full antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
        {children}
         <Analytics/>
       </body>
