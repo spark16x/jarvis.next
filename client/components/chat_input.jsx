5844389b6eb0f2677c311ad05b7cdfe9fda69d73
Spark16x <spark2009971@gmail.com>
2025-06-14 11:21:40 +0530

    update chat_input.jsx

--- client/components/chat_input.jsx
+++ client/components/chat_input.jsx
@@ -1,5 +1,5 @@
 export default function ChatInput({className}) {
   return (<div className={className} > 
-  <input type="text" placeholder="Type to chat" className=" w-full h-[50px] absolute bottom-[20px]" ></input>
+  <input type="text" placeholder="Type to chat" className=" w-full h-[50px] absolute bottom-[20px]" onChange={(e) => console.log(e.target.value)} ></input>
   </div>)
 }
