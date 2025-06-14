65f05f86d4ef1069d506f8e2b737b6440e48d131
Spark16x <spark2009971@gmail.com>
2025-06-14 10:12:43 +0530

    update

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -1,4 +1,4 @@
-import ChatInput from '@/components/chat_input.js';
+import ChatInput from '@/components/chat_input.jsx';
 
 export default function Chat() {
   return (<div className="bg-gray-900 w-screen h-screen flex" > <ChatInput className="" /></div>)
