0f4e4d082f9f038337b2e96ef90f17ff99bcda73
Spark16x <spark2009971@gmail.com>
2025-06-15 10:58:57 +0530

    update

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -1,5 +1,5 @@
 import ChatInput from '@/components/chat_input.jsx';
 
 export default function Chat() {
-  return (<div className="bg-zinc-800 w-full h-full flex justify-center-safe" > <ChatInput  className="w-1/2 h-full relative" onSendMessage={ input => alert(input)} /></div>)
+  return (<div className="bg-zinc-800 w-full h-full flex justify-center-safe" > <ChatInput  className="w-1/2 h-full relative" onSendMessage={(i)=>{}} /></div>)
 }
