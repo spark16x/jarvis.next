e4ba77e99f4a2875ed08a10ead892b55f3fd2647
Spark16x <spark2009971@gmail.com>
2025-06-15 12:05:09 +0530

    update page.jsx

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -9,8 +9,8 @@ import { redirect } from 'next/navigation';
 
 export default function Chat() {
   return (<div className="bg-zinc-800 w-full h-full flex justify-center-safe" >
-<ChatContainer classname="w-1/3 absolute left-1/3"/>
-  <ChatInput  className="w-1/3 h-full absolute left-1/3 "  />
+<ChatContainer className="w-1/3 absolute left-1/3"/>
+  <ChatInput  className="w-1/3 h-full relative "  />
       <ProfileDropdown avatarSrc="https://lh3.googleusercontent.com/a/ACg8ocKcC-LWcKIo-UKyA3pQ93FSPJWCyyaroaLFetoEZ0OuSm6uL49b=s96-c" userName = "Pratham vig" options ={[{ label: "profile", onClick: ()=>{ redirect('/')   } }  ]} 
   position = "top-left" className = '' /></div>)
 }
