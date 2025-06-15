35234b7c7b633ed04a0f56737f287e3e9e8f5d62
Spark16x <spark2009971@gmail.com>
2025-06-15 10:36:54 +0530

    update chat_input.jsx

--- client/components/chat_input.jsx
+++ client/components/chat_input.jsx
@@ -7,7 +7,10 @@ export default function ChatInput({ className  }) {
   
   return (<div className={className} > 
   <form >
+    <div className="rounded-full">
   <textarea  placeholder="Type to chat" className=" w-full h-[50px] absolute bottom-[20px] bg-zinc-700 rounded-full"  onChange={(e) => setInput(e.target.innerHtml)} >{input}</textarea>
+  </div>
+
   </form>
   </div>)
 }
