9b20c2e5ae56cee3c92a0250205a88593790d833
Spark16x <spark2009971@gmail.com>
2025-06-15 11:05:38 +0530

    update

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -1,5 +1,10 @@
 import ChatInput from '@/components/chat_input.jsx';
+import ProfileDropdown from '@/components/ProfileDropdown.jsx';
 
 export default function Chat() {
-  return (<div className="bg-zinc-800 w-full h-full flex justify-center-safe" > <ChatInput  className="w-1/2 h-full relative" onSendMessage={(i)=>{}} /></div>)
+  return (<div className="bg-zinc-800 w-full h-full flex justify-center-safe" >
+    <ProfileDropdown avatarSrc="https://lh3.googleusercontent.com/a/ACg8ocKcC-LWcKIo-UKyA3pQ93FSPJWCyyaroaLFetoEZ0OuSm6uL49b=s96-c" userName = "User" options ={[]} 
+  position = "top-left" className = '' />
+  <ChatInput  className="w-1/2 h-full relative" onSendMessage={(i)=>{}} /></div>)
 }
+
