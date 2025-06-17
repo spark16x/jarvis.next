5322f868e10ae6d18ad439557debf404f62cb11b
Spark16x <spark2009971@gmail.com>
2025-06-17 19:38:19 +0530

    update ChatContainer.jsx

--- client/components/ChatContainer.jsx
+++ client/components/ChatContainer.jsx
@@ -29,12 +29,12 @@ export default function ChatContainer({ children, className = '' }) {
     >
       <div className="flex flex-col space-y-4">
         { children}
-        {children && (
+         {/*children && (
           <div className="text-center text-zinc-400 mt-20">
             <p>Start chatting with Jarvis!</p>
             <p>Type your first message below.</p>
           </div>
-        )}
+         )*/}
       </div>
       <div className="h-2"></div>
     </div>
