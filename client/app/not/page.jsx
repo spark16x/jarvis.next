228bd74664076815b74b0f0b963e2b99a1340b29
Spark16x <spark2009971@gmail.com>
2025-06-23 10:24:09 +0530

    update page.jsx

--- client/app/not/page.jsx
+++ client/app/not/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState, useEffect } from 'react'
 import { subscribeUser, unsubscribeUser, sendNotification } from './actions'
 import PushNotificationManager from '@/components/PushNotificationManager.jsx';
