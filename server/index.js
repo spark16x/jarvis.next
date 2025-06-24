2d0bc3a3755bfaca2ded8f83a4ba01a076ad6fb4
Spark16x <spark2009971@gmail.com>
2025-06-24 12:17:04 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -656,7 +656,7 @@ app.post('/subscribe',async (req, res) => {
   
   try {
     let user = await client.query(`
- INSERT INTO public.notification(id, sub, ip, user agent)
+ INSERT INTO public.notification(id, sub, ip, "user agent")
 VALUES(gen_random_uuid(),'${serializedSub},'${ip}','${userAgent}')
 RETURNING *`);
 
