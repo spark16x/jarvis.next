f7df5ef75a5e6cea051b1f5514c7c6d7008e10fd
Spark16x <spark2009971@gmail.com>
2025-06-26 09:17:26 +0530

    update page.jsx

--- client/app/notify/page.jsx
+++ client/app/notify/page.jsx
@@ -21,7 +21,7 @@ export default function Notify() {
                 type="email"
                 id="email"
                 className="shadow-sm p-[5px] focus:ring-blue-500 focus:border-blue-500 block w-full h-[50px] text-base border-gray-700 rounded-md bg-[#0000005e] text-white"
-                placeholder="Your Email"
+                placeholder="Your Message"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 /> 
