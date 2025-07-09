a8f22cdb705e4ecdcd59cd656a5c0b49575d1bc1
Spark16x <spark2009971@gmail.com>
2025-07-09 21:37:23 +0530

    update page.jsx

--- client/app/auth/signup/page.jsx
+++ client/app/auth/signup/page.jsx
@@ -23,14 +23,14 @@ const signupPage = () => {
       return;
     }
     
-    fetch('https://jarvis-rose-zeta.vercel.app/auth/signup',{
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({name,email,password,provider:'manual'})
-}).then((v)=>{ 
-  console.log(v) ;
-  redirect('/auth/login')
-})
+    fetch('https://jarvis-rose-zeta.vercel.app/auth/signup', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password, provider: 'manual' })
+    }).then((v) => {
+      console.log(v);
+      redirect('/auth/login')
+    })
   };
   
   return (
@@ -45,10 +45,18 @@ const signupPage = () => {
 
           <div className="mt-4 space-y-3">
            
+           { /* Google */ }
+
+
+{ /* Facebook */ }
+
+           
             {/* Google OAuth */}
-            <Link href="/auth/google" className="block btn oauth-btn google-btn w-full flex items-center justify-center gap-2 py-2 px-3 rounded-md border border-gray-700 font-semibold text-gray-800 bg-white hover:bg-gray-100 transition-colors">
-              <FontAwesomeIcon icon={faGoogle} className="w-5 h-5" />
-              Continue with Google
+            <Link href="/auth/google" className="">
+              <button className="btn bg-white text-black border-[#e5e5e5]">
+                <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                     Login with Google
+                </button>
             </Link>
 
             {/* GitHub OAuth */}
@@ -58,9 +66,11 @@ const signupPage = () => {
             </Link>
 
             {/* Facebook OAuth */}
-            <Link href="/auth/facebook" className="block btn oauth-btn facebook-btn w-full flex items-center justify-center gap-2 py-2 px-3 rounded-md border border-gray-700 font-semibold text-white bg-blue-600 hover:bg-blue-500 transition-colors">
-              <FontAwesomeIcon icon={faFacebookF} className="w-5 h-5" />
-              Continue with Facebook
+            <Link href="/auth/facebook" className="">
+              <button className="btn bg-[#1A77F2] text-white border-[#005fd8]">
+                <svg aria-label="Facebook logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="white" d="M8 12h5V8c0-6 4-7 11-6v5c-4 0-5 0-5 3v2h5l-1 6h-4v12h-6V18H8z"></path></svg>
+                  Login with Facebook
+              </button>
             </Link>
 
             {/* Instagram OAuth */}
