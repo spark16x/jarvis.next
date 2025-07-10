09e54258737f391698626d9a16489be36f693501
Spark16x <spark2009971@gmail.com>
2025-07-10 16:22:19 +0530

    update page.jsx

--- client/app/auth/login/page.jsx
+++ client/app/auth/login/page.jsx
@@ -60,7 +60,7 @@ const LoginPage = () => {
             </Link>
             
             {/*<div className="border-t border-gray-700 my-4"></div>*/}
-            <p className="  text-gray-400 mb-2">Or continue with</p>
+           <div className="divider">OR</div>
             
             <form action={handleSubmit}  className="mt-4 space-y-3">
             <div>
