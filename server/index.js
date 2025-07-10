401cb1dcdd32a664f52f6a8fb268d608d65f97b6
Spark16x <spark2009971@gmail.com>
2025-07-10 16:26:34 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ import FormData from 'form-data';
 import cors from 'cors';
 import { Pool } from 'pg'
 import webpush from 'web-push';
-import { WebSocketServer } from 'ws';
 
 // config dotenv
 dotenv.config();
@@ -29,7 +28,6 @@ dotenv.config();
 // define most importent varables
 let app = express();
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
 let PORT = process.env.PORT || 3000;
 let apiKey = process.env.GEMINI_API_KEY;
 let genAI = new GoogleGenAI(apiKey);
@@ -61,38 +59,7 @@ webpush.setVapidDetails(
   VAPID_PRIVATE_KEY
 );
 
-// =======================
-// 🔌 WebSocket Setup
-// =======================
-wss.on('connection', (ws) => {
-  console.log("✅ WebSocket connected");
-  
-  
-  ws.on('message', (msg) => {
-    const { messages } = JSON.parse(msg);
-    console.log(`bot : ${messages}`);
-    
-    let response = await genAI.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: messages,
-      config: {
-        temperature: 0.7,
-        topP: 0.95,
-        topK: 40,
-        systemInstruction
-      }
-    });
-    console.log(response.text);
-    response=response.text;
-    
-    ws.send(JSON.stringify({ response }));
-  });
-  
-  ws.on('close', () => {
-    console.log("🔌 WebSocket disconnected");
-    
-  });
-});
+
 
 // let model = genAI.getGenerativeModel({
 //   model: "gemini-2.0-flash",
