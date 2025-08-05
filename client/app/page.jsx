350325eaaaedf7b6ebd2540f99cff46d27cfba0f
Spark16x <spark2009971@gmail.com>
2025-08-05 16:35:59 +0530

    update page.jsx

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -123,7 +123,7 @@ export default function Home() {
       let p = SplitText.create(".hero-p", { mask: "words" });
       // use selectors...
       timeline.from(hero.chars, { y: '50', duration: 0.3, stagger: 0.1 });
-      timeline.from(p.chars, { y: '50', duration: 0.05, stagger: 0.05 });
+      timeline.from(p.chars, { y: '50', opacity: 0, duration: 0.05, stagger: 0.05 });
       timeline.from('.hero-li', { scale: 0, duration: 0.5, stagger: 0.05 })
     },
     []); // <-- scope for selector text (optional)
